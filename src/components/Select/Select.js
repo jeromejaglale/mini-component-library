1c1fbe153d87292ff1816f547687c1d178ce1200
Jerome Jaglale <jerome.jaglale@gmail.com>
2021-12-03 15:10:22 -0800

    Style select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,51 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const Wrapper = styled.select`
+	background-color: ${COLORS.transparentGray35};
+	padding: 12px 52px 12px 16px;
+	font-size: 16px;
+
+	border-radius: 8px;
+	border-style: solid;
+	border-color: ${COLORS.transparentGray35};
+	appearance: none;
+max-width: min-content;
+color: ${COLORS.gray700};
+	// text-overflow: '';
+	// display: inline;
+	// text-indent: 5px;
+
+&:hover {
+	border-color: ${COLORS.primary};
+}
+`;
+
+const SelectIcon = styled(Icon)`
+position: absolute;
+top: 0px;
+right: 18px;
+`;
+
+const IconWrapper = styled.div`
+position: relative;
+display: inline;
+text-overflow: hidden;
+
+`;
+
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+  	<IconWrapper>
+    	<SelectIcon id='chevron-down' size='24'/>
+    <Wrapper value={value} onChange={onChange}>
       {children}
-    </select>
+    </Wrapper>
+    </IconWrapper>
   );
 };
 
